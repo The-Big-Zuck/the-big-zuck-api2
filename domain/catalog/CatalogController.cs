d5bffcdc9639a6753e3ecff746bd1e26123e1c13
Garin Spurlock <garinspurlock1@gmail.com>
2023-04-05 21:43:39 -0400

    api: add rating to catalog

--- domain/catalog/CatalogController.cs
+++ domain/catalog/CatalogController.cs
@@ -1,16 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using The.Big.Zuck.Domain.Catalog;
 
+
+HttpGet("{id:int}")]
+
+
+
 namespace The.Big.Zuck.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class CatalogController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok("Hello world!")
+        
+    public IActionResult Get(int id)
+    {
+        var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+        item.Id = id;
+        return Ok(item);
+
+    }
+
+    [HttpPost("{id:int}/ratings")]
+    public IActionResult PostRating(int id, Rating rating)
+    {
+        var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+        item.Id = id;
+        item.AddRating(rating);
+        return Ok(item);
+    }
+    public IActionResult Post(Item item)
+    {
+        return Created("/catalog/42", item);
+    }
+
+            return Ok(item);
         }
     }
+    
 }
--- domain/catalog/Rating.cs
+++ domain/catalog/Rating.cs
@@ -6,6 +6,43 @@ namespace The.Big.Zuck.Domain.Catalog
         public string UserName { get; set; }
         public string Review { get; set; }
     }
+
+
+public string Brand { get; set; }
+public decimal Price { get; set; }
+public list<Rating> Ratings { get; set; } = new list<Rating>();
+
+public void AddRating(Rating rating)
+{
+   this.Ratings.Add(rating);
+}
+
+public Item(string name, string description, string brand, decimal price)
+{
+   if(string.IsNullorEmpty(name))
+   {
+      throw new ArgumentException("Name cannot be null.");
+   }
+
+   if(string.IsNullorEmpty(description))
+   {
+      throw new ArgumentException("Description cannot be null.");
+   }
+
+   if(string.IsNullorEmpty(brand))
+   {
+      throw new ArgumentException("Brand cannot be null.");
+   }
+
+   if(price < 0.00m)
+   {
+      throw new ArgumentException("Price cannot be negative.");
+   }
+
+   this.Name = name;
+   this.Description = description;
+   this.Brand = brand;
+   this.Price = price;
 }
 
 public Rating(int stars, string userName, string review)
@@ -24,11 +61,4 @@ public Rating(int stars, string userName, string review)
    this.UserName = userName;
    this.Review = review;
 }
-public string Brand { get; set; }
-public decimal Price { get; set; }
-public list<Rating> Ratings { get; set; } = new list<Rating>();
-
-public void AddRating(Rating rating)
-{
-   this.Ratings.Add(rating);
 }
